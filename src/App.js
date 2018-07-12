4214f96746103d5f40bc9e2ead1b4434203110ee
Alonsop <alonsop@uakami.com>
2018-07-11 21:26:25 -0400

    Redo the App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Person from './Person/Person';
+
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi I'm a React app</h1>
+        <p>This Really working</p>
+        <button>Toggle Person</button>
+        <Person />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
