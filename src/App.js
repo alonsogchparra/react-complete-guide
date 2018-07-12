d9897c50f0be559ddc9efd68030eb21a2ebdffac
Alonsop <alonsop@uakami.com>
2018-07-11 21:31:53 -0400

    Adding state to the App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { id: 'weqwdasd', name: 'Bruce', age: 30 },
+      { id: 'weqwdasy', name: 'Selina', age: 28 },
+      { id: 'weqwdast', name: 'Alfred', age: 50 },
+      { id: 'weqwdasr', name: 'Dick', age: 24 },
+      { id: 'weqwdase', name: 'Jason', age: 22 },
+      { id: 'weqwdasw', name: 'Timothy', age: 20 },
+      { id: 'weqwdasq', name: 'Damian', age: 15 },
+    ],
+    otherState: 'Something',
+    showPersons: false
+  }
+
   render () {
     return (
       <div className="App">
