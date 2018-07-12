adfd016b341cad15a552a7780e49809e4b21c3b5
Alonsop <alonsop@uakami.com>
2018-07-11 21:42:12 -0400

    Props added to the Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,9 @@ import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
-      <p>Hi I'm a Person!!!</p>
+    <div className="Person">
+      <p>Hi I'm {props.name} and I'm {props.age} years old!!</p>
+      <input type="text" />
     </div>
   );
 }
