467329accd3df735f87191ea2d5f83fa7ec1f574
Alonsop <alonsop@uakami.com>
2018-07-11 21:53:04 -0400

    Adding the onClick handler with the props click from App

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ import './Person.css';
 const person = (props) => {
   return (
     <div className="Person">
-      <p>Hi I'm {props.name} and I'm {props.age} years old!!</p>
+      <p onClick={props.click}>Hi I'm {props.name} and I'm {props.age} years old!!</p>
       <input type="text" />
     </div>
   );
