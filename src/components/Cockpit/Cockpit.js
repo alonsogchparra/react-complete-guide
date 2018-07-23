8f9994f89bd81c25fd7abfb08eb88fa43bf68157
Alonsop <alonsop@uakami.com>
2018-07-23 17:58:08 -0400

    new component added Cockpit

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
+
+const cockpit = (props) => {
+  const assignedClasses = [];
+
+  let btnClass = classes.Button;
+
+  if (props.showPersons) {
+    btnClass = [classes.Button, classes.Red].join(' ');
+  }
+
+  if ( props.persons.length <= 2 ) {
+    assignedClasses.push( classes.red );
+  }
+  if ( props.persons.length <= 1 ) {
+    assignedClasses.push( classes.bold );
+  }
+
+  return (
+    <Aux>
+      <h1>{props.appTitle}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button
+        className={btnClass}
+        onClick={props.clicked}>Toggle Persons</button>
+      <button onClick={props.login}>Log in</button>
+    </Aux>
+  );
+};
+
+export default cockpit;
