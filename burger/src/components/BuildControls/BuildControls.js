ae03a06cb7923c5e53cd1a089b50c08ea1c127ce
Alonsop <alonsop@uakami.com>
2018-07-30 15:43:32 -0400

    Adding The component BuildControls and file style

---
+++ burger/src/components/BuildControls/BuildControls.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+]
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    <p>Current Price: <strong></strong></p>
+    {controls.map(ctrl => (
+      <BuildControl 
+        key={ctrl.label}
+        label={ctrl.label}
+        type={ctrl.type}
+         />
+    ))}
+    <button 
+      className={classes.OrderButton}
+      disabled={!props.purchasable}>ORDER NOW</button>
+  </div>
+);
+
+export default buildControls;
