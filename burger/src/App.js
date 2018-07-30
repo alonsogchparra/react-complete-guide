ba55161b063a754af7397fb0eb57c41e486e8512
Alonsop <alonsop@uakami.com>
2018-07-30 15:37:42 -0400

    BurgerBuilder added on App

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
-
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout />
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
