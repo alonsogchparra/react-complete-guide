cf69b516dd73d912fff16a668aad2e5a6b7de59c
Alonsop <alonsop@uakami.com>
2018-07-31 10:38:52 -0400

    Handlers added, onClick to remove ingredients and disabled props to disable the button when there is not an ingredient

--- burger/src/components/BuildControls/BuildControl/BuildControl.js
+++ burger/src/components/BuildControls/BuildControl/BuildControl.js
@@ -7,7 +7,8 @@ const buildControl = (props) => (
     <div className={classes.Label}>{props.label}</div>
     <button
       className={classes.Less} 
-      disabled={props.disabled}>Less</button>
+      disabled={props.disabled}
+      onClick={props.removed}>Less</button>
     <button
       className={classes.More}
       onClick={props.added}>More</button>
