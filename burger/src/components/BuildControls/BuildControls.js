1877207308c2fb4cd31b7fc7314dff7a528c5471
Alonsop <alonsop@uakami.com>
2018-07-31 22:27:44 -0400

    Adding props on BuildControls and updating component

--- burger/src/components/BuildControls/BuildControls.js
+++ burger/src/components/BuildControls/BuildControls.js
@@ -4,28 +4,28 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-  { label: 'Salad', type: 'salad' },
-  { label: 'Bacon', type: 'bacon' },
-  { label: 'Cheese', type: 'cheese' },
-  { label: 'Meat', type: 'meat' },
-]
+  { label: 'Salad', type: 'salad'},
+  { label: 'Bacon', type: 'bacon'},
+  { label: 'Cheese', type: 'cheese'},
+  { label: 'Meat', type: 'meat'}
+];
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
-    <p>Current Price: <strong></strong></p>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
     {controls.map(ctrl => (
       <BuildControl 
-        key={ctrl.label}
+        key={ctrl.label} 
         label={ctrl.label}
         type={ctrl.type}
         added={() => props.ingredientAdded(ctrl.type)}
-        removed={() => props.ingredientRemove(ctrl.type)}
-        disabled={props.disabled[ctrl.type]}
-         />
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]} />
     ))}
     <button 
       className={classes.OrderButton}
-      disabled={!props.purchasable}>ORDER NOW</button>
+      disabled={!props.purchasable}
+      onClick={props.ordered}>ORDER NOW</button>
   </div>
 );
 
