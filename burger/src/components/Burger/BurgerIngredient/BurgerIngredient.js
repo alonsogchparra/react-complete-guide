99d14d4571740ac1698185fffcf7053df753aa5c
Alonsop <alonsop@uakami.com>
2018-08-01 13:20:04 -0400

    BurgerIngredient component fixed

--- burger/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -3,17 +3,14 @@ import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.css';
 
-
 class BurgerIngredient extends Component {
   render () {
-
     let ingredient = null;
 
-    switch(this.props.type) {
+    switch (this.props.type) {
       case ('bread-bottom'):
         ingredient = <div className={classes.BreadBottom}></div>;
         break;
-
       case ('bread-top'):
         ingredient = (
           <div className={classes.BreadTop}>
@@ -36,11 +33,13 @@ class BurgerIngredient extends Component {
         break;
       default:
         ingredient = null;
+  
     }
+  
     return ingredient;
   }
 }
-
+ 
 BurgerIngredient.propTypes = {
   type: PropTypes.string.isRequired
 }
