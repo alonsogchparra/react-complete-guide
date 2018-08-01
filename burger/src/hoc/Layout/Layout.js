846c4ae2870c73ee914d2e947457c60a9b8b9e4c
Alonsop <alonsop@uakami.com>
2018-08-01 11:13:25 -0400

    Updating the Layout component with Toolbar and SideDrawer, also adding some methods for SideDrawer component

--- burger/src/hoc/Layout/Layout.js
+++ burger/src/hoc/Layout/Layout.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
 import Aux from '../Aux/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
+
+  state = {
+    showSideDrawer: false
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  sideDrawerToogleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer};
+    });
+  }
+
   render () {
     return (
       <Aux>
-        <div>Toolbar</div>
-        <div>Sidedrawer</div>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          close={this.sideDrawerClosedHandler}
+        />
         <main>
           {this.props.children}
         </main>
