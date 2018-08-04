6c80351866d3e278367f857563eb0b63ae6c7650
Alonsop <alonsop@uakami.com>
2018-08-04 10:40:39 -0400

    Adding componenntDidMount and axios to the project to get the posts and adding to Post component

--- posts/src/containers/Blog/Blog.js
+++ posts/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './Blog.css';
 import Post from '../../components/Post/Post';
@@ -6,13 +7,48 @@ import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 
 class Blog extends Component {
+
+  state = {
+    posts: [],
+    postSelected: null,
+    error: false
+  };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        const posts = response.data.slice(0,4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          }
+        })
+        this.setState({posts: updatedPosts});
+      })
+      .catch(error => {
+        this.setState({ error: true });
+      });
+  }
+
   render () {
+
+    let posts = <p>Something is wrong!</p>;
+
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post
+                  key={post.id} 
+                  title={post.title}
+                  author={post.author} />
+      });
+  
+    }
+
     return (
       <div>
         <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
+          {posts}
         </section>
         <section>
           <FullPost />
