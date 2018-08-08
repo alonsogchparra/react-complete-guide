057106d221d86ad615c674bdd2d7f2d6754761c4
Alonsop <alonsop@uakami.com>
2018-08-08 15:27:17 -0400

    Adding on App BrowserRouter

--- posts/src/App.js
+++ posts/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
