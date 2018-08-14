f82ebce3d237c66fc19ac23f3b2767d3284f6d6c
Alonsop <alonsop@uakami.com>
2018-08-14 16:25:32 -0400

    Order component fixed

--- burger/src/components/Order/Order.js
+++ burger/src/components/Order/Order.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './Order.css'
+import classes from './Order.css';
 
 const order = (props) => {
   const ingredients = [];
@@ -8,7 +8,7 @@ const order = (props) => {
   for (let ingredientName in props.ingredients) {
     ingredients.push(
       {
-        name:ingredientName,
+        name: ingredientName,
         amount: props.ingredients[ingredientName]
       }
     );
@@ -19,8 +19,8 @@ const order = (props) => {
               style={
                 {
                   textTransform: 'capitalize',
-                  display:'inline-block',
-                  margin:'0 8px',
+                  display: 'inline-block',
+                  margin: '0 8px',
                   border: '1px solid #ccc',
                   padding: '5px'
                 }
@@ -36,6 +36,6 @@ const order = (props) => {
       <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
     </div>
   );
-}
+};
 
 export default order;
