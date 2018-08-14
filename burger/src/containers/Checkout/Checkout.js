5eab7c06f77d8c77f7cfb91a6e23e0a47fe59b35
Alonsop <alonsop@uakami.com>
2018-08-14 11:16:32 -0400

    Building the Checkout container

---
+++ burger/src/containers/Checkout/Checkout.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
+
+class Checkout extends Component {
+
+  state = {
+    ingredients: null,
+    price: 0
+  };
+
+
+  componentWillMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    let price = 0;
+    for (let param in query.entries()) {
+      // ['salad', '1']
+      if (param[0] === 'price') {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
+    }
+    this.setState({ ingredients: ingredients, price: price});
+  }
+
+  checkoutCancelled = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinued = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
+  render () {
+    return (
+      <div>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelled}
+          checkoutContinued={this.checkoutContinued} />
+          <Route
+            path={this.props.match.path + '/contact-data'}
+            render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.price} />)} />
+      </div>
+    )
+  }
+}
+
+export default Checkout;
