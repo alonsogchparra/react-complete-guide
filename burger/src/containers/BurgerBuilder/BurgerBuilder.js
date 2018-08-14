a240edfbc36ee4e56cdacf50ba87896ea640917c
Alonsop <alonsop@uakami.com>
2018-08-14 09:30:57 -0400

    Updating purchaseContinueHandler with encodeURIComponent

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -104,27 +104,17 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Alonso Parra',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '5145',
-          country: 'Venezuela'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
+
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i)+ '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   }
 
   render () {
