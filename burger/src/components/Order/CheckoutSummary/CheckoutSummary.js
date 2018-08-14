f34425791883446bf808ea3905ec498bf4c268a7
Alonsop <alonsop@uakami.com>
2018-08-14 16:23:05 -0400

    Some elements corrected

--- burger/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import classes from './CheckoutSummary.css';
 import Burger from '../../Burger/Burger';
-import Button from '../../../components/UI/Button/Button';
+import Button from '../../UI/Button/Button';
 
 const checkoutSummary = (props) => {
   return (
     <div className={classes.CheckoutSummary}>
       <h1>We hope it tastes well!</h1>
-      <div style={{ width: '100%', margin: 'auto'}}>
+      <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
       </div>
       <Button 
