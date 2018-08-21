e32f3611d87a1f814d55abbbb760b5cb4c19d3bc
Alonsop <alonsop@uakami.com>
2018-08-21 16:12:24 -0400

    Updating the index with redux and react-redux

--- redux_counter/src/index.js
+++ redux_counter/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import counterReducer from './store/reducers/counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  ctr: counterReducer
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
