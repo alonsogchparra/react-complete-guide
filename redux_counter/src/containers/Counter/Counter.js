811c74cce77270aa98dae9b092ebbeddd511e900
Alonsop <alonsop@uakami.com>
2018-08-21 16:10:47 -0400

    Adding mapStateToProps and mapDispatchToProps on Counter and add some props

--- redux_counter/src/containers/Counter/Counter.js
+++ redux_counter/src/containers/Counter/Counter.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/actions';
 
 import './Counter.css';
 
@@ -9,14 +11,29 @@ class Counter extends Component {
   render () {
     return (
       <div>
-        <CounterOutput />
-        <CounterControl label="Increment" />
-        <CounterControl label="Decrement" />
-        <CounterControl label="Add 10" />
-        <CounterControl label="Subtract 15" />
+        <CounterOutput value={this.props.counter} />
+        <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+        <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+        <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
+        <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter} />
       </div>
     );
   }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+  return {
+    counter: state.ctr.counter
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+    onAddCounter: () => dispatch({ type: actionTypes.ADD, value: 10 }),
+    onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, value: 15 })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
