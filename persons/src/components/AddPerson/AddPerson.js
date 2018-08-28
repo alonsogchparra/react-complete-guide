da2323de1340c6cb918094aad564a807a99b7b4b
Alonsop <alonsop@uakami.com>
2018-08-28 15:00:35 -0400

    Adding inputs, button, state and handlers to AddPerson

--- persons/src/components/AddPerson/AddPerson.js
+++ persons/src/components/AddPerson/AddPerson.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 class AddPerson extends Component {
+
+  state = {
+    name: '',
+    age: ''
+  };
+
+  onNameChangeHandler = (event) => {
+    this.setState({ name: event.target.value });
+  }
+
+  onAgeChangeHandler = (event) => {
+    this.setState({ age: event.target.value });
+  }
+
   render () {
     return (
       <div>
-        AddPerson
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={this.onNameChangeHandler}
+           />
+        <input
+          type="number"
+          placeholder="Age"
+          onChange={this.onAgeChangeHandler}
+           />
+        <button>Add Person</button>
       </div>
     )
   }
