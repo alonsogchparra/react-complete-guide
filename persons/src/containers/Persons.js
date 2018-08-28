616eeb21747d391e820e6c6bb2f105df91c9287a
Alonsop <alonsop@uakami.com>
2018-08-28 16:15:22 -0400

    Adding redux to Persons and adding props to AddPerson and Person

--- persons/src/containers/Persons.js
+++ persons/src/containers/Persons.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import AddPerson from '../components/AddPerson/AddPerson';
 import Person from '../components/Person/Person';
+import * as actionTypes from '../store/actions';
 
 class Persons extends Component {
   render () {
     return (
       <div>
-        Persons
-        <AddPerson />
-        <Person />
+        <AddPerson personAdded={this.props.onAddedPerson} />
+        {this.props.persons.map(person => (
+          <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            clicked={() => this.props.onRemovePerson(person.id)} />
+        ))}
       </div>
     )
   }
 }
 
-export default Persons;
+const mapStateToProps = state => {
+  return {
+    persons: state.persons
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddedPerson: (name, age) => dispatch ({ type: actionTypes.ADD_PERSON, personData: { name: name, age: age }}),
+    onRemovePerson: (id) => dispatch ({ type: actionTypes.REMOVE_PERSON, personId: id })
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
