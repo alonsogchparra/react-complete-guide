578936cd7c9115e794d8516548804292d9dfbb0e
Alonsop <alonsop@uakami.com>
2018-08-28 15:04:03 -0400

    Creating reducer

---
+++ persons/src/store/reducer.js
@@ -0,0 +1,34 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+  persons: []
+};
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+
+    case actionTypes.ADD_PERSON:
+      const newPerson = {
+        id: Math.random(),
+        name: action.personData.name,
+        age: action.personData.age
+      };
+      return {
+        ...state,
+        persons: newPerson
+      }
+
+    case actionTypes.REMOVE_PERSON:
+      return {
+        ...state,
+        persons: state.persons.filter(person => person.id !== action.personId)
+      }
+
+    default:
+      return state;
+  }
+
+}
+
+export default reducer;
