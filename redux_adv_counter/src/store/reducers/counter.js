bb8c62a29092a96eae9f7a71769b41094b49ca39
Alonso Parra <alonsogparra@gmail.com>
2018-09-17 23:16:19 -0400

    reducers/counter updated

--- redux_adv_counter/src/store/reducers/counter.js
+++ redux_adv_counter/src/store/reducers/counter.js
@@ -20,6 +20,18 @@ const reducer = (state = initialState, action) => {
         counter: state.counter - 1
       }
 
+    case actionTypes.ADD:
+      return {
+        ...state,
+        counter: state.counter + action.value
+      }
+
+    case actionTypes.SUBTRACT:
+      return {
+        ...state,
+        counter: state.counter - action.value
+      }
+
     default:
       return state;
 
