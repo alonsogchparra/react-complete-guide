79a31ded05993fcbf1fda900ac0e63bf29ddd476
Alonso Parra <alonsogparra@gmail.com>
2018-09-17 22:26:48 -0400

    building the counter reducer with increment and decrement actions

---
+++ redux_adv_counter/src/store/reducers/counter.js
@@ -0,0 +1,29 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  counter: 0
+};
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+
+    case actionTypes.INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+
+    case actionTypes.DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+
+    default:
+      return state;
+
+  }
+}
+
+export default reducer;
