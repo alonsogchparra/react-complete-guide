824c4be91e52fd71877453b686c14597871ff938
Alonso Parra <alonsogparra@gmail.com>
2018-09-17 23:02:17 -0400

    Building the result reduce

---
+++ redux_adv_counter/src/store/reducers/result.js
@@ -0,0 +1,29 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  results: []
+};
+
+const reducer = (state = initialState, action) => {
+  
+  switch (action.type) {
+    
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: action.result })
+      }
+
+    case actionTypes.DELETE_RESULT:
+      const updatedArray = state.results.filter(result => result.id !== action.id);
+      return {
+        ...state,
+        results: updatedArray
+      }
+
+    default:
+      return state;
+  }
+}
+
+export default reducer;
