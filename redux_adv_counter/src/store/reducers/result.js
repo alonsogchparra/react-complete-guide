c520e2450ad5d2bae2c0af753c6728eb357377d7
Alonso Parra <alonsogparra@gmail.com>
2018-09-17 23:35:57 -0400

    reducers updated with utility

--- redux_adv_counter/src/store/reducers/result.js
+++ redux_adv_counter/src/store/reducers/result.js
@@ -1,25 +1,25 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   results: []
 };
+
+const deleteResult = (state, action) => {
+ const updatedArray = state.results.filter(result => result.id !== action.id);
+ return updateObject(state, {results: updatedArray});
+};
+
 
 const reducer = (state = initialState, action) => {
   
   switch (action.type) {
     
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({ id: new Date(), value: action.result })
-      }
+      return updateObject(state, {results: state.results.concat({ id: new Date(), value: action.result })});
 
     case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter(result => result.id !== action.id);
-      return {
-        ...state,
-        results: updatedArray
-      }
+      return deleteResult(state, action);
 
     default:
       return state;
