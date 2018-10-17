fd0f1a0fb4b46236a1095260c5200b4dd0967323
Alonso Parra <alonsogparra@gmail.com>
2018-10-17 10:24:39 -0400

    adding authReducer to index

--- burger/src/index.js
+++ burger/src/index.js
@@ -10,10 +10,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order: orderReducer
+  order: orderReducer,
+  auth: authReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
