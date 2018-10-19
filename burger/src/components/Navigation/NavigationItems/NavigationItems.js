ba88eb78b4ffeb903c9dc2a7b69a741b1b995b8b
Alonso Parra <alonsogparra@gmail.com>
2018-10-19 14:33:32 -0400

    Reorder new updated props

--- burger/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,10 +6,10 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-    {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
-    {!props.isAuthenticated 
-      ? <NavigationItem link="/auth">Authenticate</NavigationItem> 
-      : <NavigationItem link="/logout">Logout</NavigationItem>}
+    { props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null }
+    { !props.isAuthenticated 
+      ? <NavigationItem link="/auth">Authenticate</NavigationItem>
+      : <NavigationItem link="/logout">Logout</NavigationItem> }
   </ul>
 );
 
