a65c4986ef1d4a51aa632ffede271fb15607d9d6
Alonso Parra <alonsogparra@gmail.com>
2018-10-22 17:09:16 -0400

    updating auth with utility on InputHandler

--- burger/src/containers/Auth/Auth.js
+++ burger/src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import Input from '../../components/UI/Input/Input';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import Button from '../../components/UI/Button/Button';
 import * as actions from '../../store/actions/index';
+import { updateObject } from '../../shared/utility';
 import classes from './Auth.css';
 
 class Auth extends Component {
@@ -83,15 +84,13 @@ class Auth extends Component {
   }
 
   inputChangedHandler = (event, controlName) => {
-    const updatedControls = {
-      ...this.state.controls,
-      [controlName]: {
-        ...this.state.controls[controlName],
+    const updatedControls = updateObject(this.state.controls, {
+      [controlName]: updateObject(this.state.controls[controlName], {
         value: event.target.value,
         valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
         touched: true
-      }
-    };
+      })
+    });
     this.setState({ controls: updatedControls });
   }
 
