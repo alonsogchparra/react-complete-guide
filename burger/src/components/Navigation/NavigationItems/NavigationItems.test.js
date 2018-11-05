1ebf92c90c5f285918c0b659fa450d359c3544c5
Alonso Parra <alonsogparra@gmail.com>
2018-11-05 10:57:10 -0400

    Adding NavigationItems test checking if shows two elements when is not authenticated

---
+++ burger/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({ adapter: new Adapter() });
+
+describe('<NavigationItems />', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  })
+
+  it('should render two <NavigationItem /> elements if is not authenticated', () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+});
