ff280e3462b3ad06dbe5bb4aead073b52b25bc9b
Alonso Parra <alonsogparra@gmail.com>
2018-11-15 09:51:57 -0400

    getInitialProps on auth index added

--- nextjs/pages/auth/index.js
+++ nextjs/pages/auth/index.js
@@ -4,9 +4,18 @@ import User from '../../components/user';
 
 const authIndexPage = (props) => (
   <div>
-    <h1>Auth Page</h1>
+    <h1>Auth Page of {props.appName}</h1>
     <User name="Alonso" age={29} />
   </div>
 );
+
+authIndexPage.getInitialProps = (context => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: 'Super App - (Auth)'});
+    }, 1000);
+  });
+  return promise;
+});
 
 export default authIndexPage;
