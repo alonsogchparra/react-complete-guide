2be20fffd215e307569b68d2054fa033b9701062
Alonso Parra <alonsogparra@gmail.com>
2018-11-15 09:40:04 -0400

    Change functional component to a class and added getInitialProps on it

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 
-const indexPage = () => (
-  <div>
-    <h1>Main Page</h1>
-    <Link href="/auth"><a>Auth</a></Link>
-  </div>
-);
+class IndexPage extends Component {
 
-export default indexPage;
+  static getInitialProps (context) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: 'Super App'});
+      }, 1000);
+    });
+    return promise
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Main Page of {this.props.appName}</h1>
+        <Link href="/auth"><a>Auth</a></Link>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
