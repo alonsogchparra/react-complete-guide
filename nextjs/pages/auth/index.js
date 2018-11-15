88e299000a7814ac78d5e815fa78a95804fbd51c
Alonso Parra <alonsogparra@gmail.com>
2018-11-15 09:27:27 -0400

    Adding the user component into auth index

--- nextjs/pages/auth/index.js
+++ nextjs/pages/auth/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import User from '../../components/user';
+
 const authIndexPage = (props) => (
   <div>
     <h1>Auth Page</h1>
+    <User name="Alonso" age={29} />
   </div>
 );
 
