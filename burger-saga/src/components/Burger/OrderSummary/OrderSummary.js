388ff11efafea6955a44bdbad3829f3a08a330f3
Alonso Parra <alonsogparra@gmail.com>
2018-11-26 09:24:41 -0400

    Uploading the burger project with redux-saga dependency added

---
+++ burger-saga/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Aux/Aux';
+import Button from '../../UI/Button/Button';
+
+// Will turning this functional component into a statefull component
+// to make sure when this component have to be active or have to update
+
+class OrderSummary extends Component {
+
+  // This could be a functional component, doesn't have to be a class
+
+  render () {
+
+    const ingredientSummary = Object.keys(this.props.ingredients)
+    .map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
+    return (
+      <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+    </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
