2f32796ca6e26261ac4d10093b6d13e2e4159bb3
Alonso Parra <alonsogparra@gmail.com>
2018-11-29 16:02:39 -0400

    Added the new actionTypes and generators on index saga

--- burger-saga/src/store/sagas/index.js
+++ burger-saga/src/store/sagas/index.js
@@ -1,8 +1,12 @@
 import { takeEvery, all, takeLatest } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import { logoutSaga } from './auth';
+import { logoutSaga, checkAuthTimeoutSaga ,authUserSaga } from './auth';
 
 export function* watchAuth() {
-  yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+  yield all([
+    takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+    takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+    takeEvery(actionTypes.AUTH_USER, authUserSaga )
+  ]);
 }
