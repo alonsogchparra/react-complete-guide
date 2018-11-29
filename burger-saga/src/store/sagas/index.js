bbeee6e385117c972b50a667a9405199ee9e2c36
Alonso Parra <alonsogparra@gmail.com>
2018-11-29 16:12:59 -0400

    actioType and generator about checkAuthStateSaga added on index saga

--- burger-saga/src/store/sagas/index.js
+++ burger-saga/src/store/sagas/index.js
@@ -1,12 +1,13 @@
 import { takeEvery, all, takeLatest } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import { logoutSaga, checkAuthTimeoutSaga ,authUserSaga } from './auth';
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
 
 export function* watchAuth() {
   yield all([
     takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
     takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
-    takeEvery(actionTypes.AUTH_USER, authUserSaga )
+    takeEvery(actionTypes.AUTH_USER, authUserSaga),
+    takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
   ]);
 }
