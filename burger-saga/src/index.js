b161cf9226c4d8a812b050a7393b47210b35987d
Alonso Parra <alonsogparra@gmail.com>
2018-11-29 10:20:55 -0400

    Adding saga Middleware to the index

--- burger-saga/src/index.js
+++ burger-saga/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
 import App from './App';
@@ -11,6 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { watchAuth } from './store/sagas';
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
@@ -18,9 +20,15 @@ const rootReducer = combineReducers({
   auth: authReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk, sagaMiddleware)
+  ));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
   <Provider store={store}>
