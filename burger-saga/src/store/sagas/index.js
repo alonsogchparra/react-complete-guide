1a52a0943356157b4250bd7b39aefbc8d6e9c09b
Alonso Parra <alonsogparra@gmail.com>
2018-11-30 16:17:38 -0400

    watchOrder added

--- burger-saga/src/store/sagas/index.js
+++ burger-saga/src/store/sagas/index.js
@@ -3,6 +3,7 @@ import { takeEvery, all, takeLatest } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
 import { initIngredientsSaga } from './burgerBuilder';
+import { fetchOrderSaga, purchaseBurgerSaga } from './order';
 
 export function* watchAuth() {
   yield all([
@@ -15,4 +16,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
   yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+  yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrderSaga);
 }
