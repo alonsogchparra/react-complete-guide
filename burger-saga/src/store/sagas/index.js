eade0d673a4b008a59b31cbb974d3a959b42c9e4
Alonso Parra <alonsogparra@gmail.com>
2018-11-30 11:30:07 -0400

    adding new generator about burgerBuilder

--- burger-saga/src/store/sagas/index.js
+++ burger-saga/src/store/sagas/index.js
@@ -2,6 +2,7 @@ import { takeEvery, all, takeLatest } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
+import { initIngredientsSaga } from './burgerBuilder';
 
 export function* watchAuth() {
   yield all([
@@ -10,4 +11,8 @@ export function* watchAuth() {
     takeEvery(actionTypes.AUTH_USER, authUserSaga),
     takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
   ]);
+}
+
+export function* watchBurgerBuilder() {
+  yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
 }
