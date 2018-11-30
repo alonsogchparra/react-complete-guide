9c8b5e52cc663db92c31ea534cb3cdfae6e2f716
Alonso Parra <alonsogparra@gmail.com>
2018-11-30 11:27:58 -0400

    Action creator initIngredients updated

--- burger-saga/src/store/actions/burgerBuilder.js
+++ burger-saga/src/store/actions/burgerBuilder.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
 
 export const addIngredient = ( name ) => {
   return {
@@ -29,13 +28,7 @@ export const fetchIngredientsFailed = () => {
 };
 
 export const initIngredients = () => {
-  return dispatch => {
-    axios.get('/ingredients.json')
-    .then(response => {
-      dispatch(setIngredients(response.data));
-    })
-    .catch(error => {
-      dispatch(fetchIngredientsFailed());
-    });
+  return {
+    type: actionTypes.INIT_INGREDIENTS
   };
 };
