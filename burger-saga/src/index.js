702f0c06450f4d6b9b7320f55754926c96c6aba2
Alonso Parra <alonsogparra@gmail.com>
2018-11-30 16:10:45 -0400

    watchOrder added on index

--- burger-saga/src/index.js
+++ burger-saga/src/index.js
@@ -12,7 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
-import { watchAuth, watchBurgerBuilder } from './store/sagas';
+import { watchAuth, watchBurgerBuilder, watchOrder } from './store/sagas';
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
@@ -30,6 +30,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 sagaMiddleware.run(watchAuth);
 sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
 
 const app = (
   <Provider store={store}>
